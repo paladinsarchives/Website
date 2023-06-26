8e56f8b43feac806f7b2b1a0107a4aee569afcaf
paladinsarchives <119296247+paladinsarchives@users.noreply.github.com>
2023-06-26 13:14:23 -0500

    Update Changeable.js

--- Changeable.js
+++ Changeable.js
@@ -4,7 +4,7 @@ let pdf = "https://drive.google.com/file/d/1jq_3fb5-zndVfN7hzX30RZ_RT5GKqPkz/vie
 
         // Sign up numbers
 // Brawn
-let brawn = 5.5;
+let brawn = 6.5;
 
 // Scout
 let scout = 3;
